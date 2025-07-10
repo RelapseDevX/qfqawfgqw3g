d5450965c2b71567dcb1015e93cab9bf31f390fc
RelapseDevX <callumlovesdad@gmail.com>
2025-07-10 15:59:43 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -13,7 +13,7 @@ local tweenService   = cloneref(UserSettings().GetService(game,"TweenService"));
 local players        = cloneref(UserSettings().GetService(game,"Players"));
 local localPlayer    = players.LocalPlayer
 local mouse          = cloneref(localPlayer:GetMouse())
-getgenv().menu           = game:GetObjects("rbxassetid://10334577211")[1]
+getgenv().menu           = game:GetObjects("rbxassetid://11140140476")[1]
 local Tween = loadstring(clonefunction(game.HttpGet)(game,"https://pastebin.com/raw/GRtM1ysK"))()
 
 local notifications1 = {}
